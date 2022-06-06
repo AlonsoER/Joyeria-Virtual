3a8fa00e7f53d6bfb115a85c81c768468973e959
AlonsoER <alonsoer2343@gmail.com>
2022-06-06 17:53:23 -0600

    estructurando la seccion de catalogo

--- gulpfile.js
+++ gulpfile.js
@@ -30,6 +30,45 @@ function dev( done ){
     done();
 }
 
+function imagenes( done ){
+    const opciones ={
+        optimozationLevel: 3
+    }
+    
+        src('src/img/**/*.{png,jpg}')
+        .pipe( plumber())
+        .pipe( cache( imagemin( opciones)))
+        .pipe( dest('build/img'))
+        done();
+    }
+    
+    function versionWebp( done ){
+        const opciones = {
+            quality: 50
+        };
+    
+        src('src/img/**/*.{png,jpg}')
+        .pipe( plumber())
+        .pipe( webp(opciones))
+        .pipe( dest('build/img'))
+        done();
+    }
+    
+    function versionAvif( done ){
+        const opciones = {
+            quality: 50
+        };
+    
+        src('src/img/**/*.{png,jpg}')
+        .pipe( plumber())
+        .pipe( avif(opciones))
+        .pipe( dest('build/img'))
+        done();
+    }
+
 exports.css = css;
 exports.javascript = javascript;
-exports.dev = parallel(css, javascript, dev);
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.imagenes = imagenes;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, css, javascript, dev);
