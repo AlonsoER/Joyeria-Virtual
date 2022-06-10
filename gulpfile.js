3df2f9fe8b3279b0d7a07365513029b3e4cd3f7b
AlonsoER <alonsoer2343@gmail.com>
2022-06-10 11:31:35 -0600

    Creando seccion catalogo

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,71 @@
-const {src, dest, parallel, watch} = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const plumber = require('gulp-plumber');
+const { src, dest, parallel, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 // imagenes
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
-const cache = require('gulp-cache');
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+const cache = require("gulp-cache");
 
-
-
-function css( done ){
-    src('src/scss/**/*.scss')
-    .pipe( plumber())
-    .pipe( sass())
-    .pipe( dest('build/css'))
+function css(done) {
+    src("src/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(dest("build/css"));
     done();
 }
 
-function javascript( done ){
-    src('src/js/**/*.js')
-    .pipe( dest('build/js'))
+function javascript(done) {
+    src("src/js/**/*.js").pipe(dest("build/js"));
     done();
 }
 
-function dev( done ){
-    watch('src/scss/**/*.scss', css);
-    watch('src/js/**/*.js', javascript);
+function dev(done) {
+    watch("src/scss/**/*.scss", css);
+    watch("src/js/**/*.js", javascript);
     done();
 }
 
-function imagenes( done ){
-    const opciones ={
-        optimozationLevel: 3
-    }
-    
-        src('src/img/**/*.{png,jpg}')
-        .pipe( plumber())
-        .pipe( cache( imagemin( opciones)))
-        .pipe( dest('build/img'))
-        done();
-    }
-    
-    function versionWebp( done ){
-        const opciones = {
-            quality: 50
-        };
-    
-        src('src/img/**/*.{png,jpg}')
-        .pipe( plumber())
-        .pipe( webp(opciones))
-        .pipe( dest('build/img'))
-        done();
-    }
-    
-    function versionAvif( done ){
-        const opciones = {
-            quality: 50
-        };
-    
-        src('src/img/**/*.{png,jpg}')
-        .pipe( plumber())
-        .pipe( avif(opciones))
-        .pipe( dest('build/img'))
-        done();
-    }
+function imagenes(done) {
+    const opciones = {
+    optimozationLevel: 3,
+};
+
+    src("src/img/**/*.{png,jpg}")
+    .pipe(plumber())
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest("build/img"));
+    done();
+}
+
+function versionWebp(done) {
+    const opciones = {
+    quality: 50,
+};
+
+    src("src/img/**/*.{png,jpg}")
+    .pipe(plumber())
+    .pipe(webp(opciones))
+    .pipe(dest("build/img"));
+    done();
+}
+
+function versionAvif(done) {
+    const opciones = {
+    quality: 50,
+};
+
+    src("src/img/**/*.{png,jpg}")
+    .pipe(plumber())
+    .pipe(avif(opciones))
+    .pipe(dest("build/img"));
+    done();
+}
 
 exports.css = css;
 exports.javascript = javascript;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
 exports.imagenes = imagenes;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, css, javascript, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, css ,javascript, dev);
