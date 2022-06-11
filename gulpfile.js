256a99bfcb3482673267a9056995dd8c8507fffe
AlonsoER <alonsoer2343@gmail.com>
2022-06-11 12:25:39 -0600

    Terminando seccion catálogo

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const plumber = require("gulp-plumber");
 // imagenes
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
-const avif = require("gulp-avif");
 const cache = require("gulp-cache");
 
 function css(done) {
@@ -51,21 +50,8 @@ function versionWebp(done) {
     done();
 }
 
-function versionAvif(done) {
-    const opciones = {
-    quality: 50,
-};
-
-    src("src/img/**/*.{png,jpg}")
-    .pipe(plumber())
-    .pipe(avif(opciones))
-    .pipe(dest("build/img"));
-    done();
-}
-
 exports.css = css;
 exports.javascript = javascript;
 exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif;
 exports.imagenes = imagenes;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, css ,javascript, dev);
+exports.dev = parallel(imagenes, versionWebp, css ,javascript, dev);
