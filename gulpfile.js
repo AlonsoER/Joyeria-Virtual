beadcf61a9fea56c404e621ffa1e2f874e7a18d6
AlonsoER <alonsoer2343@gmail.com>
2022-06-12 10:52:06 -0600

    Comprimiendo codigo css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const { src, dest, parallel, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require('autoprefixer');
+const postcss    = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
 
 // imagenes
 const imagemin = require("gulp-imagemin");
@@ -9,8 +14,11 @@ const cache = require("gulp-cache");
 
 function css(done) {
     src("src/scss/**/*.scss")
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write('.'))
     .pipe(dest("build/css"));
     done();
 }
